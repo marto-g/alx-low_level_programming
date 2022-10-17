ab990abf5939d630cc4aca125351e4fed3c545ed
marto-g <gathimartin@gmail.com>
2022-10-17 03:32:40 -0700

    task7

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ *
+ *
+ * Return: Always 0 (success)
+ */
+
+
+
+int main(void)
+
+{
+
+		int a;
+
+		for (a = 0; a < 10; a++)
+
+			printf("%d", a);
+
+				putchar('\n');
+
+					return (0);
+
+}
