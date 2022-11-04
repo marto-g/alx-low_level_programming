ef41e6142de987bf8d6b6ceafee39a62282caa00
marto-g <gathimartin@gmail.com>
2022-11-04 05:49:02 -0700

    task 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,6 +11,23 @@ int _strlen_recursion(char *s)
 	return (0);
 }
 /**
+ *pal_checker - check if s is palindrome.
+ *@s: string base address.
+ *@i: left index.
+ *@j: right index.
+ *Return: 1 if s is palindrome, 0 otherwise.
+ */
+int pal_checker(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (pal_checker(s, i + 1, j - 1));
+	else
+		return (0);
+}
+/**
  * is_palindrome - check if s is palindrome
  * @s: base address for string.
  *
